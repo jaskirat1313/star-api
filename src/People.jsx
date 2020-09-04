2d44294b682ca1975e679ba9c9afe92a63bf5eed
jaskirat <40401279+jaskirat1313@users.noreply.github.com>
2020-09-05 05:19:56 +0530

    commit

--- src/People.jsx
+++ src/People.jsx
@@ -3,13 +3,17 @@ import React from "react";
 const People = (props) => {
   return (
     <>
-      <div className="col-md-4 col-10 mx-auto bg-light">
+      <div className="col-md-4 col-10 my-2 ">
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">{props.tittle}</h5>
-            <p className="card-text">Skin_color: {props.skin_color}</p>
-            <p className="card-text">Gender: {props.gender}</p>
-            <p className="card-text">Homeworld: {props.homeworld}</p>
+            <p className="card-text">
+              Skin_color: {props.skin_color}
+              <br />
+              Gender: {props.gender}
+              <br />
+              Homeworld: {props.homeworld}
+            </p>
           </div>
         </div>
       </div>
